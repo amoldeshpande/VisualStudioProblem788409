ad37a072c821c4a86048acc965b133a412b0622e
amol <amol.deshpande@outlook.com>
2019-11-01 09:21:48 -0700

    simple thing to repro problem

--- printenv.cmd
+++ printenv.cmd
@@ -1,5 +1,11 @@
 @echo off
-set
+REM set
+
+echo path is 
 echo %PATH%
+
+echo ------------------------------------------------------------------------------------
+
+echo libpath is 
 
 echo %LIBPATH%
